54b25ff48d480497f956d3330c21573b11d175d1
Lech1337 <adamskikamil0@gmail.com>
2025-12-04 14:37:27 +0100

    04.12.2025

---
+++ projekty/mysql/konkurs.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>WOLONTARIAT SZKOLNY</title>
+        <link rel="stylesheet" href="styles2.css">
+    </head>
+    <body>
+        <header>
+            <h1>KONKURS - WOLONTARIAT SZKOLNY</h1>
+        </header>
+
+        <div id="lewy">
+            <h3>Konkursowe nagrody</h3>
+            <button onclick="location.reload()">Losuj nowe nagrody</button>
+
+
+            <table>
+                <head>
+                    <tr>
+                        <th>Nr</th>
+                        <th>Nazwa</th>
+                        <th>Opis</th>
+                        <th>Wartość</th>
+                    </tr>
+                </head>
+            </table>
+        </div>
+
+        <div id="prawy">
+            <img src="puchar.png" alt="Puchar dla wolontariusza">
+            <h4>Polecane linki</h4>
+            <ul>
+                <li><a href="kw1.png">Kwerenda1</a></li>
+                <li><a href="kw2.png">Kwerenda2</a></li>
+                <li><a href="kw3.png">Kwerenda3</a></li>
+                <li><a href="kw4.png">Kwerenda4</a></li>
+            </ul>
+        </div>
+
+        <footer>
+            Numer zdającego:1</a>
+        </footer>
+    </body>
+</html>
